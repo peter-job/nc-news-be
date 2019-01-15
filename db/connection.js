7b1733f52c0900f3479c7fc587dcbfb9afc3e711
ptjob1 <ptjob6@gmail.com>
2019-01-15 11:08:03 +0000

    Create db/connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,7 @@
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
+const dbConfig = require('../knexfile')[ENV];
 
+const connection = knex(dbConfig);
+
+module.exports = connection;
