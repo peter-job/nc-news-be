0009a3e2a950ce2d03d6fd6bb1db81de2e768445
ptjob1 <ptjob6@gmail.com>
2019-01-15 15:49:16 +0000

    /api/topics POST:201 complete with error handling

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = require('express')();
+const bodyparser = require('body-parser');
 const topicsRouter = require('./routes/topicsRouter');
+const { handle400 } = require('./error');
+
+app.use(bodyparser.json());
 
 app.use('/api/topics', topicsRouter);
+app.use(handle400);
 
 module.exports = app;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -7,7 +7,7 @@ const connection = require('../db/connection');
 
 const request = supertest(app);
 
-describe('/api', () => {
+describe('/', () => {
   // hooks
   beforeEach(() => connection.migrate
     .rollback()
@@ -16,15 +16,28 @@ describe('/api', () => {
   after(() => {
     connection.destroy();
   });
-  // test
-  describe('/topics', () => {
-    it('GET status:200 should respond with an array of topics', () => request
-      .get('/api/topics')
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.topics).to.be.an('array');
-        expect(body.topics[0]).to.haveOwnProperty('slug');
-        expect(body.topics[0]).to.haveOwnProperty('description');
-      }));
+  // <----- describe 404 /
+  it('GET status 404', () => request.get('/amadeupendpoint').expect(404));
+  describe('/api', () => {
+    describe('/topics', () => {
+      it('GET status:200 should respond with an array of topics', () => request
+        .get('/api/topics')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.topics).to.be.an('array');
+          expect(body.topics[0]).to.haveOwnProperty('slug');
+          expect(body.topics[0]).to.haveOwnProperty('description');
+        }));
+      it('POST status:201 should respond with object from body', () => request
+        .post('/api/topics')
+        .send({ slug: 'garbage', description: 'all things garbage' })
+        .expect(201)
+        .then(({ body }) => {
+          expect(body.topic).to.be.an('object');
+          expect(body.topic).to.haveOwnProperty('slug');
+          expect(body.topic).to.haveOwnProperty('description');
+        }));
+      // <----- describe 404 /api*
+    });
   });
 });
